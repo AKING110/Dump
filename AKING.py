0e1e129b2e775a298b5bc0dc2ba5bd0aa0f01cb3
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-05-30 22:54:33 +0500

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,19 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
+    os.system('xdg-open https://facebook.com/groups/1017905562448002/')
+if __name__ == "__main__":
+	try:
+		__import__("Dump").Menu()
+	except Exception as e:
+		exit(str(e))
+elif bit == '32bit':
+    os.system('xdg-open https://facebook.com/groups/1017905562448002/')
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
