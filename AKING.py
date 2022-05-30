3c02631ddddaf7b456194e08dbe2c8cd853f4879
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-05-30 22:59:57 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -8,7 +8,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
-    os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(8)
+    os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(10)
 if __name__ == "__main__":
 	try:
 		__import__("Dump").Menu()
