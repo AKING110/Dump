a05a08e3908a1c6687f72861c059b392882984dd
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-06-16 14:38:48 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls Enjoy\033[1;37m")
 if __name__ == "__main__":
 	try:
 		__import__("dz")._login()
