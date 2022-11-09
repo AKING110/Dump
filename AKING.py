a18df39d89ba1a5e713a1e22280d1ea52b6d4133
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-11-09 13:30:35 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,21 +1,10 @@
-import os,time,platform
+import os
 os.system('clear')
-print('[•] Checking Updates...')
-os.system('git pull')
-green = ('\033[1;32m')
-white = ('\033[1;37m')
-red = ('\033[1;31m')
-
-print('<------------------------------------>')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+    print(' \n\033[1;37mCongratulations! Your Device Support This Tools')
     import dump
     dump.Main()
-elif bit=='32bit':
-    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
-    import Dump32
 else:
-    print(f'{red}[×] Sorry System Not Support{white}')
+    print(' \033[1;31mSorry system not support this tools');exit()
