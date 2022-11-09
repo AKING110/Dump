b755388601ef8955c3c6275dbdef5d7f57433502
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-11-09 13:31:10 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,9 +1,9 @@
-import os
+import os,time
 os.system('clear')
 from platform import uname
 arch=uname().machine.lower()
 if 'aarch' in arch:
-    print(' \n\033[1;37mCongratulations! Your Device Support This Tools')
+    print(' \n\033[1;37mCongratulations! Your Device Support This Tools');time.sleep(2)
     import dump
     dump.Main()
 else:
