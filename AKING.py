14d19fbe8f5ea0f30026499466cdd5ef1707bd5e
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-12-14 14:14:23 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -4,8 +4,7 @@ os.system('xdg-open https://facebook.com/groups/351076900316263/')
 from platform import uname
 arch=uname().machine.lower()
 if 'aarch' in arch:
-    print(' \n\033[1;37mCongratulations! Your Device Support This Tools');time.sleep(2)
-    import dump
-    dump.Main()
+    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
+    os.system('chmod 777 dump && ./dump')
 else:
-    print(' \033[1;31mSorry system not support this tools');exit()
+    print(' \033[1;31m Sorry system or device not support this tools');exit()
