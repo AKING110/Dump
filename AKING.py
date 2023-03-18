0ecf1a343a55e9c1a12cff54fd3ccc28986f3f7e
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-03-18 07:41:30 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,6 +5,6 @@ from platform import uname
 arch=uname().machine.lower()
 if 'aarch' in arch:
     print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    os.system('chmod 777 dump && ./dump')
+    import dump
 else:
     print(' \033[1;31m Sorry system or device not support this tools');exit()
